8b73d60c87e3048a33f2b24c05c43735b100633c
K2976 <sanghikartik2976@gmail.com>
2026-01-21 00:16:54 +0530

    Added special effect/font style for special food items

--- src/components/MenuItem.jsx
+++ src/components/MenuItem.jsx
@@ -1,6 +1,6 @@
 export default function MenuItem({ name, price, special }) {
   return (
-    <div className={`item ${special ? "special" : ""}`}>
+    <div className={`item ${special == true ? "special" : ""}`}>
       <span>{name}</span>
       <span className="price">{price}</span>
     </div>
